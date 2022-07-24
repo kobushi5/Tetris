a0efdb8e092c47f7d31610d4e0ae7122ffebba9c
kobushi5 <107742406+kobushi5@users.noreply.github.com>
2022-07-24 16:23:02 +0900

    Add files via upload

---
+++ Tetris (1).js
@@ -0,0 +1,472 @@
+const mino = {
+    I_mino: [
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0]
+    ],
+    I_mino0: [
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0]
+    ],
+    I_mino1: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [2, 2, 2, 2],
+        [0, 0, 0, 0]
+    ],
+    I_mino2: [
+        [0, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0]
+    ],
+    I_mino3: [
+        [0, 0, 0, 0],
+        [2, 2, 2, 2],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+
+    O_mino: [
+        [0, 0, 0, 0],
+        [0, 2, 2, 0],
+        [0, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+    O_mino0: [
+        [0, 0, 0, 0],
+        [0, 2, 2, 0],
+        [0, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+    O_mino1: [
+        [0, 0, 0, 0],
+        [0, 2, 2, 0],
+        [0, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+    O_mino2: [
+        [0, 0, 0, 0],
+        [0, 2, 2, 0],
+        [0, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+    O_mino3: [
+        [0, 0, 0, 0],
+        [0, 2, 2, 0],
+        [0, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+
+    T_mino: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [2, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+    T_mino0: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [2, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+    T_mino1: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 2, 0],
+        [0, 2, 0, 0]
+    ],
+    T_mino2: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [2, 2, 2, 0],
+        [0, 2, 0, 0]
+    ],
+    T_mino3: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [2, 2, 0, 0],
+        [0, 2, 0, 0]
+    ],
+
+    L_mino: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 2, 0]
+    ],
+    L_mino0: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 2, 0]
+    ],
+    L_mino1: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [2, 2, 2, 0],
+        [2, 0, 0, 0]
+    ],
+    L_mino2: [
+        [0, 0, 0, 0],
+        [2, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0]
+    ],
+    L_mino3: [
+        [0, 0, 0, 0],
+        [0, 0, 2, 0],
+        [2, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+
+    J_mino: [
+        [0, 0, 0, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 2, 2, 0]
+    ],
+    J_mino0: [
+        [0, 0, 0, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 2, 2, 0]
+    ],
+    J_mino1: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 2, 2],
+        [0, 0, 0, 0]
+    ],
+    J_mino2: [
+        [0, 0, 0, 0],
+        [0, 0, 2, 2],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0]
+    ],
+    J_mino3: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 2, 2, 2],
+        [0, 0, 0, 2]
+    ],
+
+    S_mino: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 2, 0],
+        [0, 0, 2, 0]
+    ],
+    S_mino0: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 2, 0],
+        [0, 0, 2, 0]
+    ],
+    S_mino1: [
+        [0, 0, 0, 0],
+        [0, 2, 2, 0],
+        [2, 2, 0, 0],
+        [0, 0, 0, 0]
+    ],
+    S_mino2: [
+        [0, 0, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 2, 0],
+        [0, 0, 2, 0]
+    ],
+    S_mino3: [
+        [0, 0, 0, 0],
+        [0, 2, 2, 0],
+        [2, 2, 0, 0],
+        [0, 0, 0, 0]
+    ],
+
+    Z_mino: [
+        [0, 0, 0, 0],
+        [0, 0, 2, 0],
+        [0, 2, 2, 0],
+        [0, 2, 0, 0]
+    ],
+    Z_mino0: [
+        [0, 0, 0, 0],
+        [0, 0, 2, 0],
+        [0, 2, 2, 0],
+        [0, 2, 0, 0]
+    ],
+    Z_mino1: [
+        [0, 0, 0, 0],
+        [2, 2, 0, 0],
+        [0, 2, 2, 0],
+        [0, 0, 0, 0]
+    ],
+    Z_mino2: [
+        [0, 0, 0, 0],
+        [0, 0, 2, 0],
+        [0, 2, 2, 0],
+        [0, 2, 0, 0]
+    ],
+    Z_mino3: [
+        [0, 0, 0, 0],
+        [2, 2, 0, 0],
+        [0, 2, 2, 0],
+        [0, 0, 0, 0]
+    ]
+    
+};
+let stage = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+const canvas = document.createElement("canvas");
+canvas.width = stage[0].length * 25;
+canvas.height = stage.length * 25;
+document.body.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+//いらんかも↓
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, stage[0].length * 25, stage.length * 25);
+
+function sleep(waitSec, callbackFunc) {
+
+    // 経過時間（秒）
+    var spanedSec = 0;
+
+    // 1秒間隔で無名関数を実行
+    var id = setInterval(function () {
+
+        spanedSec++;
+
+        // 経過時間 >= 待機時間の場合、待機終了。
+        if (spanedSec >= waitSec) {
+
+            // タイマー停止
+            clearInterval(id);
+
+            // 完了時、コールバック関数を実行
+            if (callbackFunc) callbackFunc();
+        }
+    }, 250);
+
+}
+let count = 0;
+
+const mino_num = ["I", "O", "T", "L", "J", "S", "Z"];
+const shuffle = ([...array]) => {
+    for (let i = array.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]
+    }
+    return array;
+}
+let end = 1;
+let gameover = 0;
+let minoKaiten = 0;
+let r = 0;
+
+let setMinoEnd = 0;
+function setMino() {
+    if (gameover == 1) {
+        return;
+    }
+    setMinoEnd = 0;
+    const shuffled_mino = shuffle(mino_num);
+    console.log(shuffled_mino);
+    mino_count(fallMino);
+    function mino_count() {
+        if (gameover == 1) {
+            return;
+        }
+        if (r == 7) {
+            r = 0;
+            setMinoEnd = 1
+            return;
+        }
+        else {
+            let nowMinoNum = shuffled_mino[r];
+            //let nowMino = mino[shuffled_mino[r] + "_mino"];
+            fallMino(nowMinoNum);
+            r++;
+            let inter = setInterval(function () {
+                if (end !== 1) {
+                    return;
+                }
+                mino_count();
+                clearInterval(inter);
+            }, 100);
+        }
+    }
+    let minoInter = setInterval(function () {
+        if (setMinoEnd !== 1) {
+            return;
+        }
+        setMino();
+        clearInterval(minoInter);
+    }, 100);
+}
+
+let minoOverMin = 0;//フラグ
+let minoOverMax = 0;
+function fallMino(which) {
+    let whichMino= mino[which + "_mino" + String(minoKaiten)];
+    minoOverMin = 0;
+    minoOverMax = 0;
+    end = 0;
+    for (let y = 0; y < whichMino.length; y++) {
+        for (let x = 0; x < 4; x++) {
+            
+            if (whichMino[y][x] == 2 && count != 0) {
+                if (stage[y + count][x + 4 + mino_x - 1] == 3 || stage[y + count][x + 4 + mino_x - 1] == 5) {//左
+                    minoOverMin = 1;
+                }
+                if (stage[y + count][x + 4 + mino_x + 1] == 3 || stage[y + count][x + 4 + mino_x + 1] == 4) {//右
+                    minoOverMax = 1;
+                }
+                if (stage[y + count][x + 4 + mino_x] == 3 || stage[y + count][x + 4 + mino_x] == 4) {//右
+                    mino_x--
+                }
+                if (stage[y + count][x + 4 + mino_x] == 3 || stage[y + count][x + 4 + mino_x] == 5) {//右
+                    mino_x++
+                }
+                if (stage[y + count + 1][x + 4 + mino_x] == 1 || stage[y + count + 1][x + 4 + mino_x] == 3) {//下判定
+                    end = 1;
+                }
+                else {
+                    if (end != 1) {
+                        stage[y + count][x + 4 + mino_x] = 2;
+                    }
+                }
+            }
+        }
+    }
+    if (end == 1) {
+        for (let y = 0; y < whichMino.length; y++) {
+            for (let x = 0; x < 4; x++) {
+                if (whichMino[y][x] == 2) {
+                    stage[y + count][x + 4 + mino_x] = 3;
+                }
+            }
+        }
+        mino_x = 0;
+        mino_y = 0;
+        count = 0;
+        minoKaiten=0;
+        return;
+    }
+    count++;
+    sleep(1, function () {
+        fallMino(which);
+    });
+}
+let mino_x = 0;
+let mino_y = 0;
+
+document.addEventListener('keydown', function (e) {
+    if (e.code == "KeyS" || e.code == "ArrowDown") {
+        mino_y--;
+    }
+    if (e.code == "KeyA" || e.code == "ArrowLeft") {
+        if (minoOverMin != 1) {
+            mino_x--;
+        }
+        minoOverMax = 0;
+    }
+    if (e.code == "KeyD" || e.code == "ArrowRight") {
+        if(minoOverMax != 1){
+            mino_x++;
+        }
+        minoOverMin = 0;
+    }
+    if (e.code== "KeyE"){
+        minoKaiten++;
+        minoKaiten = minoKaiten % 4;
+    }
+    if (mino_x > 5) {
+        mino_x = 5
+    }
+    if (mino_x < -5) {
+        mino_x = -5;
+    }
+})
+let clearCount=0;
+function display() {
+
+    if (gameover == 1) {
+        ctx.fillStyle = "black";
+        ctx.font="45px cursive"
+        ctx.fillText("GAME OVER", 10, 300);
+        return;
+    }
+    for (let y = 0; y < stage.length; y++) {
+        clearCount=0;
+        for (let x = 0; x < stage[y].length; x++) {
+            if(stage[y][x]==3){
+                clearCount++;
+                if(clearCount==10){
+                    for(let i =1;i<11;i++){
+                        stage[y][i]==0;
+                    }
+                    for(let j=y;j>2;j--){
+                        for(let k=1;k<11;k++){
+                            stage[j][k]=stage[j-1][k]
+                        }
+                    }
+                }
+            }
+            if (y>2&&x==11){
+                stage[y][x]=4;
+            }
+            if (y>2&&x==0){
+                stage[y][x]=5;
+            }
+            if (stage[2][x] == 3) {
+                gameover = 1;
+            }
+            if (stage[y][x] == 0) {
+                ctx.fillStyle = "lightblue"
+                ctx.fillRect(x * 25, y * 25, 24, 24);
+            } else if (stage[y][x] == 2) {
+                stage[y][x] = 0;
+                ctx.fillStyle = "blue"
+                ctx.fillRect(x * 25, y * 25, 24, 24);
+            } else if (stage[y][x] == 3) {
+                ctx.fillStyle = "red"
+                ctx.fillRect(x * 25, y * 25, 24, 24)
+            }
+            else {
+                ctx.fillStyle = "darkblue";
+                ctx.fillRect(x * 25, y * 25, 24, 24);
+            }
+        }
+    }
+}
+display();
+setInterval(display, 250);
+setMino();
+//console.log(stage);
